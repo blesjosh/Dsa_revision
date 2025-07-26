3e462b2989c63521ae3a9fe06a874c0bfc963db1
blesjosh <blessingjoshuaoffl@gmail.com>
2025-07-26 09:11:07 +0530

    Print 1 - N

---
+++ Recursion/2_Print_1toN.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+void printN(int n, int i = 1) {
+    if (i > n) return; // Base case: if i is greater than n, stop recursion
+    cout << i << " ";   // Print the current value of i
+    printN(n, i + 1);   // Recursive call with i incremented by 1
+}
+
+int main() {
+    int n;
+    cin >> n;           // Input the value of n
+    printN(n);         // Call the function to print numbers from 1 to n
+    return 0;
+}
